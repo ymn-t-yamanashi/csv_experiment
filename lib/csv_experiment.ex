2264ec7f1fe0fd76686759703973dad6049affe8
ymn-t-yamanashi <ae86bg5@yahoo.co.jp>
2024-09-11 13:35:34 +0900

    NimbleCSV検証

--- lib/csv_experiment.ex
+++ lib/csv_experiment.ex
@@ -1,3 +1,5 @@
+NimbleCSV.define(Csv, [])
+
 defmodule CsvExperiment do
   @moduledoc """
   Documentation for `CsvExperiment`.
@@ -12,7 +14,46 @@ defmodule CsvExperiment do
       :world
 
   """
+
+  @range 1..9
+  @csv_file_name "multiplication.csv"
   def hello do
+    list = creat_multiplication_list()
+    IO.puts("------------ 掛け算リスト ------------")
+    IO.inspect(list)
+
+    IO.puts("------------ 掛け算CSV作成 ------------")
+
+    list
+    |> wite_csv!(@csv_file_name)
+
+    File.read!(@csv_file_name)
+    |> IO.inspect()
+
+    IO.puts("------------ 掛け算CSVロード ------------")
+
+    read_csv!(@csv_file_name)
+    |> IO.inspect()
+
     :world
+  end
+
+  def wite_csv!(data, path) do
+    data
+    |> Csv.dump_to_iodata()
+    |> IO.iodata_to_binary()
+    |> then(&File.write!(path, &1))
+  end
+
+  def read_csv!(path), do: File.read!(path) |> Csv.parse_string()
+
+  def creat_multiplication_list() do
+    multiplication_list = []
+
+    for x <- @range do
+      step = []
+      step = for y <- @range, do: step ++ (x * y)
+      multiplication_list ++ step
+    end
   end
 end
