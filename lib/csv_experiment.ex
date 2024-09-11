307cf1296eb21ea292235b59baf13495506dbf64
ymn-t-yamanashi <ae86bg5@yahoo.co.jp>
2024-09-11 12:44:41 +0900

    mix new csv_experiment

---
+++ lib/csv_experiment.ex
@@ -0,0 +1,18 @@
+defmodule CsvExperiment do
+  @moduledoc """
+  Documentation for `CsvExperiment`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CsvExperiment.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/csv_experiment_test.exs
@@ -0,0 +1,8 @@
+defmodule CsvExperimentTest do
+  use ExUnit.Case
+  doctest CsvExperiment
+
+  test "greets the world" do
+    assert CsvExperiment.hello() == :world
+  end
+end
